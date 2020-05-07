dab385dc3b78b5dc184ee4544af83f5188d10491
zhangke <ke.zhang@han-networks.com>
2020-05-07 15:11:24 +0800

    bub

---
+++ sort/bub/bub.go
@@ -0,0 +1,19 @@
+package bub
+
+// Bub
+func Bub(nums []int) (res []int) {
+	for i := 0; i < len(nums)-1; i++ { //处理len(nums)-1趟
+		for j := 0; j < len(nums)-1-i; j++ { // 处理第i个数据
+			if nums[j] > nums[j+1] {
+				swap(&nums[j], &nums[j+1])
+			}
+		}
+	}
+	return nums
+}
+// swap
+func swap(a, b *int) {
+	t := *a
+	*a = *b
+	*b = t
+}
--- sort/shell/shell.go
+++ sort/shell/shell.go
@@ -1,11 +1,4 @@
 package shell
-import "fmt"
-
-func main() {
-	s := []int{88, 1, 55, 45, 4546, 564, 68, 13, 1, 3654, 896}
-	s = Shell(s,len(s)/3)
-	fmt.Println(s)
-}
 
 // Shell
 func Shell(nums []int, dk int) (res []int) {
@@ -15,14 +8,14 @@ func Shell(nums []int, dk int) (res []int) {
 			return nums
 		}
 		for i := 0; i < dk; i++ { // 循环各个分组
-			for j := i; j < len(nums); j = j + dk {//循环每个分组 , 进行插入排序
-				p:=j-1
-				c:=nums[j]
-				for ;p>=0&&c<nums[p];{
-					nums[p+dk]=nums[p]
+			for j := i; j < len(nums); j = j + dk { //循环每个分组 , 进行插入排序
+				p := j - 1
+				c := nums[j]
+				for p >= 0 && c < nums[p] {
+					nums[p+dk] = nums[p]
 					p--
 				}
-				nums[p+dk]=c
+				nums[p+dk] = c
 			}
 		}
 
