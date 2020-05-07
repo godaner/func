3ac6caadd0dc95bfedb000b1b999dba8152ec64a
zhangke <ke.zhang@han-networks.com>
2020-05-07 11:38:06 +0800

    shell sort

--- sort/shell/shell.go
+++ sort/shell/shell.go
@@ -11,14 +11,21 @@ func main() {
 func Shell(nums []int, dk int) (res []int) {
 	for { // 计算dk
 		dk = dk / 2
-		for i := 0; i < dk; i++ { // 循环各个分组
-			for j := i; j < len(nums); j = j + dk {//循环每个分组
-
-			}
-		}
-		if dk == 1 {
+		if dk == 0 {
 			return nums
 		}
+		for i := 0; i < dk; i++ { // 循环各个分组
+			for j := i; j < len(nums); j = j + dk {//循环每个分组 , 进行插入排序
+				p:=j-1
+				c:=nums[j]
+				for ;p>=0&&c<nums[p];{
+					nums[p+dk]=nums[p]
+					p--
+				}
+				nums[p+dk]=c
+			}
+		}
+
 	}
 
 }
