c4a9582c0d859ad991135434d44b8ac7444fe7d8
zhangke <ke.zhang@han-networks.com>
2020-05-12 12:50:11 +0800

    bucket

--- sort/bucket.go
+++ sort/bucket.go
@@ -8,7 +8,7 @@ func Bucket(nums []int, bucketSize int) (res []int) {
 
 	// to bucket
 	for _, num := range nums {
-		p := num / bucketSize
+		p := (num - minN)/bucketSize // 映射关系
 		val := bucket[p]
 		if val == nil {
 			val = make([]int, 0)
@@ -19,6 +19,32 @@ func Bucket(nums []int, bucketSize int) (res []int) {
 	// sort
 	res = make([]int, 0)
 	for _, val := range bucket {
+		if len(val) == 0 {
+			continue
+		}
+		val = Insert(val)
+		res = append(res, val...)
+	}
+	return res
+}
+func BucketV2(nums []int, bucketSize int) (res []int) {
+	maxN := maxNumber(nums)
+	minN := minNumber(nums)
+	bucket := make(map[int][]int)
+
+	// to bucket
+	for _, num := range nums {
+		p := (num-minN) / bucketSize // 映射关系
+		val, _ := bucket[p]
+		if val == nil {
+			val = make([]int, 0)
+		}
+		val = append(val, num)
+		bucket[p] = val
+	}
+	// sort
+	for i := 0; i < (maxN-minN)/bucketSize+1; i++ {
+		val, _ := bucket[i]
 		if len(val) == 0 {
 			continue
 		}
--- sort/bucket_test.go
+++ sort/bucket_test.go
@@ -17,3 +17,16 @@ func TestBucket(t *testing.T) {
 		}
 	}
 }
+func TestBucketV2(t *testing.T) {
+	testCases=[]testCase{}
+	appendRandom(&testCases, randomTestCaseMaxNumber, randomTestCaseNum, randomTestCaseArrLen)
+	for i, testCase := range testCases {
+		res := BucketV2(testCase.src,10)
+		c := testCase.comp(testCase.res, res)
+		//t.Logf("TestBucketV2 : testCase[%v] actually res is : %v , wanna res is : %v !", i, res, testCase.res)
+		if c != 0 {
+			t.Fatalf("TestBucketV2 : testCase[%v] fail , actually res is : %v , wanna res is : %v !", i, res, testCase.res)
+			//t.Fatalf("TestBucketV2 : testCase[%v] fail !", i)
+		}
+	}
+}
