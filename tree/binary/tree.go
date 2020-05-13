d0d6166e3e3c7785cfd2160bd19985cd7bfb383b
zhangke <ke.zhang@han-networks.com>
2020-05-13 11:08:09 +0800

    tree

--- tree/binary/tree.go
+++ tree/binary/tree.go
@@ -1,87 +1,120 @@
 package binary
 
-import (
-	"strings"
-)
-
-const (
-	nilNode = "#"
-)
-
 // Tree
 type Tree struct {
 	Left, Right *Tree
-	Date        string
+	Date        int
+}
+
+// Min
+func (t *Tree) Min() (minN int) {
+	min(t, &minN)
+	return minN
+}
+
+// min
+func min(root *Tree, minN *int) {
+	if root == nil {
+		return
+	}
+	if root.Date < *minN {
+		*minN = root.Date
+	}
+	min(root.Left, minN)
+	min(root.Right, minN)
+}
+
+// Max
+func (t *Tree) Max() (maxN int) {
+	max(t, &maxN)
+	return maxN
+}
+
+// max
+func max(root *Tree, maxN *int) {
+	if root == nil {
+		return
+	}
+	if root.Date > *maxN {
+		*maxN = root.Date
+	}
+	max(root.Left, maxN)
+	max(root.Right, maxN)
 }
 
 // Pre
-func (t *Tree) Pre() (p string) {
+func (t *Tree) Pre() (p []int) {
+	p = []int{}
 	pre(t, &p)
 	return p
 }
 
 // pre
-func pre(root *Tree, p *string) {
+func pre(root *Tree, p *[]int) {
 	if root == nil {
 		return
 	}
-	*p = *p + root.Date
+	*p = append(*p, root.Date)
 	pre(root.Left, p)
 	pre(root.Right, p)
 
 }
+
 // Mid
-func (t *Tree) Mid() (m string) {
+func (t *Tree) Mid() (m []int) {
+	m = []int{}
 	mid(t, &m)
 	return m
 }
 
 // mid
-func mid(root *Tree, m *string) {
+func mid(root *Tree, m *[]int) {
 	if root == nil {
 		return
 	}
 	mid(root.Left, m)
-	*m = *m + root.Date
+	*m = append(*m, root.Date)
 	mid(root.Right, m)
 
 }
+
 // Post
-func (t *Tree) Post() (p string) {
+func (t *Tree) Post() (p []int) {
+	p = []int{}
 	post(t, &p)
 	return p
 }
 
 // post
-func post(root *Tree, p *string) {
+func post(root *Tree, p *[]int) {
 	if root == nil {
 		return
 	}
 	post(root.Left, p)
 	post(root.Right, p)
-	*p = *p + root.Date
+	*p = append(*p, root.Date)
 
 }
+
 // Build
 //  通过前序遍历建立二叉树
 //  ABDH##I##E##CF#J##G##
-func Build(nodes string) (t *Tree) {
-	ns := strings.Split(nodes, "")
+func Build(nodes []*int) (t *Tree) {
 	i := -1 // 第几次输入
-	return build(ns, &i)
+	return build(nodes, &i)
 }
 
 // build
 //  nodes　总的输入
 //  i　第几次输入
-func build(nodes []string, i *int) (t *Tree) {
+func build(nodes []*int, i *int) (t *Tree) {
 	l := len(nodes)
 	if l == 0 || l < *i {
 		return nil
 	}
 	*i = (*i) + 1
 	data := nodes[*i]
-	if data == nilNode {
+	if data == nil {
 		return nil
 	}
 	node := newTreeNode(data)
@@ -91,10 +124,10 @@ func build(nodes []string, i *int) (t *Tree) {
 }
 
 // newTreeNode
-func newTreeNode(data string) (tn *Tree) {
+func newTreeNode(data *int) (tn *Tree) {
 	return &Tree{
 		Left:  nil,
 		Right: nil,
-		Date:  data,
+		Date:  *data,
 	}
 }
--- tree/binary/tree_test.go
+++ tree/binary/tree_test.go
@@ -1,6 +1,7 @@
 package binary
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -10,29 +11,52 @@ import (
 // -1 : a < b
 type compare func(a, b interface{}) (res int)
 
+func intP(i int) (p *int) {
+	return &i
+}
+func TestW2N(t *testing.T) {
+	sss := []string{"ABDH##I##E##CF#J##G##"}
+	for index, ss := range sss {
+		fmt.Print(index, " : ")
+		for i, _ := range ss {
+			if ss[i] >= 65 {
+				fmt.Print(ss[i] - 65)
+			} else {
+				fmt.Print("#")
+			}
+		}
+
+	}
+}
 func TestTree_Pre(t *testing.T) {
 	c := func(a, b interface{}) (res int) {
-		ai, _ := a.(string)
-		bi, _ := b.(string)
-		if ai == bi {
-			return 0
+		ai, _ := a.([]int)
+		bi, _ := b.([]int)
+		for index, ain := range ai {
+			if bi[index] != ain {
+				return -1
+			}
 		}
-		return -1
+		return 0
 	}
 	testCases := []struct {
-		preSrc   string
-		wannaRes string
+		preSrc   []*int
+		wannaRes []int
 		comp     compare
 	}{
 		{
-			preSrc:   "ABDH##I##E##CF#J##G##",
-			wannaRes: "ABDHIECFJG",
+			//ABDH##I##E##CF#J##G##
+			//0137##8##4##25#9##6##
+			preSrc:   []*int{intP(0), intP(1), intP(3), intP(7), nil, nil, intP(8), nil, nil, intP(4), nil, nil, intP(2), intP(5), nil, intP(9), nil, nil, intP(6), nil, nil},
+			wannaRes: []int{0, 1, 3, 7, 8, 4, 2, 5, 9, 6},
 			comp:     c,
 		},
 	}
 	for i, testCase := range testCases {
 		tree := Build(testCase.preSrc)
 		actuallyPreRes := tree.Pre()
+		fmt.Println(tree.Mid())
+		fmt.Println(tree.Post())
 		c := testCase.comp(testCase.wannaRes, actuallyPreRes)
 		if c != 0 {
 			t.Fatalf("TestTree_Pre : testCase[%v] fail , actually res is : %v , wanna res is : %v !", i, actuallyPreRes, testCase.wannaRes)
@@ -41,21 +65,24 @@ func TestTree_Pre(t *testing.T) {
 }
 func TestTree_Mid(t *testing.T) {
 	c := func(a, b interface{}) (res int) {
-		ai, _ := a.(string)
-		bi, _ := b.(string)
-		if ai == bi {
-			return 0
+		ai, _ := a.([]int)
+		bi, _ := b.([]int)
+		for index, ain := range ai {
+			if bi[index] != ain {
+				return -1
+			}
 		}
-		return -1
+		return 0
 	}
 	testCases := []struct {
-		preSrc   string
-		wannaRes string
+		preSrc   []*int
+		wannaRes []int
 		comp     compare
 	}{
 		{
-			preSrc:   "ABDH##I##E##CF#J##G##",
-			wannaRes: "HDIBEAFJCG",
+			preSrc: []*int{intP(0), intP(1), intP(3), intP(7), nil, nil, intP(8), nil, nil, intP(4), nil, nil, intP(2), intP(5), nil, intP(9), nil, nil, intP(6), nil, nil},
+			//"HDIBEAFJCG"
+			wannaRes: []int{7, 3, 8, 1, 4, 0, 5, 9, 2, 6},
 			comp:     c,
 		},
 	}
@@ -71,21 +98,24 @@ func TestTree_Mid(t *testing.T) {
 
 func TestTree_Post(t *testing.T) {
 	c := func(a, b interface{}) (res int) {
-		ai, _ := a.(string)
-		bi, _ := b.(string)
-		if ai == bi {
-			return 0
+		ai, _ := a.([]int)
+		bi, _ := b.([]int)
+		for index, ain := range ai {
+			if bi[index] != ain {
+				return -1
+			}
 		}
-		return -1
+		return 0
 	}
 	testCases := []struct {
-		preSrc   string
-		wannaRes string
+		preSrc   []*int
+		wannaRes []int
 		comp     compare
 	}{
 		{
-			preSrc:   "ABDH##I##E##CF#J##G##",
-			wannaRes: "HIDEBJFGCA",
+			preSrc: []*int{intP(0), intP(1), intP(3), intP(7), nil, nil, intP(8), nil, nil, intP(4), nil, nil, intP(2), intP(5), nil, intP(9), nil, nil, intP(6), nil, nil},
+			//"HIDEBJFGCA"
+			wannaRes: []int{7, 8, 3, 4, 1, 9, 5, 6, 2, 0},
 			comp:     c,
 		},
 	}
@@ -95,6 +125,68 @@ func TestTree_Post(t *testing.T) {
 		c := testCase.comp(testCase.wannaRes, actuallyPreRes)
 		if c != 0 {
 			t.Fatalf("TestTree_Post : testCase[%v] fail , actually res is : %v , wanna res is : %v !", i, actuallyPreRes, testCase.wannaRes)
+		}
+	}
+}
+
+func TestTree_Max(t *testing.T) {
+	c := func(a, b interface{}) (res int) {
+		ai, _ := a.(int)
+		bi, _ := b.(int)
+		if ai==bi{
+			return 0
+		}
+		return -1
+	}
+	testCases := []struct {
+		preSrc   []*int
+		wannaRes int
+		comp     compare
+	}{
+		{
+			preSrc: []*int{intP(0), intP(1), intP(3), intP(7), nil, nil, intP(8), nil, nil, intP(4), nil, nil, intP(2), intP(5), nil, intP(9), nil, nil, intP(6), nil, nil},
+			//"HIDEBJFGCA"
+			wannaRes: 9,
+			comp:     c,
+		},
+	}
+	for i, testCase := range testCases {
+		tree := Build(testCase.preSrc)
+		actuallyRes := tree.Max()
+		c := testCase.comp(testCase.wannaRes, actuallyRes)
+		if c != 0 {
+			t.Fatalf("TestTree_Max : testCase[%v] fail , actually res is : %v , wanna res is : %v !", i, actuallyRes, testCase.wannaRes)
+		}
+	}
+}
+
+func TestTree_Min(t *testing.T) {
+	c := func(a, b interface{}) (res int) {
+		ai, _ := a.(int)
+		bi, _ := b.(int)
+		if ai==bi{
+			return 0
+		}
+		return -1
+	}
+	testCases := []struct {
+		preSrc   []*int
+		wannaRes int
+		comp     compare
+	}{
+		{
+			preSrc: []*int{intP(0), intP(1), intP(3), intP(7), nil, nil, intP(8), nil, nil, intP(4), nil, nil, intP(2), intP(5), nil, intP(9), nil, nil, intP(6), nil, nil},
+			//"HIDEBJFGCA"
+			wannaRes: 0,
+			comp:     c,
+		},
+	}
+	for i, testCase := range testCases {
+		tree := Build(testCase.preSrc)
+		actuallyRes := tree.Min()
+		c := testCase.comp(testCase.wannaRes, actuallyRes)
+		if c != 0 {
+			t.Fatalf("TestTree_Min : testCase[%v] fail , actually res is : %v , wanna res is : %v !", i, actuallyRes, testCase.wannaRes)
 		}
 	}
 }
